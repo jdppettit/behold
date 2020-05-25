dc6b4cb41ff6974e67fb3e98fd159c957be0ef6f
Joseph Pettit <jpettit@pettitservers.com>
2020-05-25 17:43:40 -0500

    improve email notification content / subject

--- lib/observer/lib/notification/email.ex
+++ lib/observer/lib/notification/email.ex
@@ -1,15 +1,38 @@
 defmodule Observer.Notification.Email do
   require Logger
 
+  @from Application.get_env(:observer, :email_notification_from, "behold@example.com")
+
   import Bamboo.Email
 
   def send(check, alert, :down) do
     new_email(
       to: alert.target,
-      from: "behold@example.com",
-      subject: "Behold Monitoring: Check for #{check.target} is #{check.state}",
-      html_body: "The check has failed.",
-      text_body: "The check has failed."
+      from: @from,
+      subject: "[Behold Monitoring]: #{check.name} is DOWN",
+      html_body: 
+      """
+      <p>Name: #{check.name}</p>
+      <p>Target: #{check.target}</p>
+      <p>Type: #{check.type}</p>
+      <p>ID: #{check.id}</p>
+      <p>
+        Depending on your alert settings you will continue to be notified
+        until resolution or will only receive another notificaiton once
+        the issue has resolved.
+      </p>
+      """,
+      text_body: 
+      """
+      Name: #{check.name}
+      Target: #{check.target}
+      Type: #{check.type}
+      ID: #{check.id}
+      
+      Depending on your alert settings you will continue to be notified
+      until resolution or will only receive another notificaiton once
+      the issue has resolved.
+      """
     )
     |> Observer.Common.Mailer.deliver_now()
   end
@@ -17,10 +40,22 @@ defmodule Observer.Notification.Email do
   def send(check, alert, :up) do
     new_email(
       to: alert.target,
-      from: "behold@example.com",
-      subject: "Behold Monitoring: Check for #{check.target} is #{check.state}",
-      html_body: "The check has returned to normal.",
-      text_body: "The check has returned to normal."
+      from: @from,
+      subject: "[Behold Monitoring]: #{check.name} has RECOVERED",
+      html_body: 
+      """
+      <p>Name: #{check.name}</p>
+      <p>Target: #{check.target}</p>
+      <p>Type: #{check.type}</p>
+      <p>ID: #{check.id}</p>
+      """,
+      text_body: 
+      """
+      Name: #{check.name}
+      Target: #{check.target}
+      Type: #{check.type}
+      ID: #{check.id}
+      """
     )
     |> Observer.Common.Mailer.deliver_now()
   end
